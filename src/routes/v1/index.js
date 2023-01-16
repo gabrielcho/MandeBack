446bd12090a91018aadca6dc89970d69f34a7388
Gabriel Arango <gabrielarango15@gmail.com>
2023-01-15 22:18:14 -0500

    Se maquetan paths de rutas y controladores

---
+++ src/routes/v1/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const clientRoute = require('./client.route');
+const workerRoute = require('./worker.route');
+
+const router = express.Router();
+
+router.use('/client', clientRoute);
+router.use('/worker', workerRoute);
+
+module.exports = router;
