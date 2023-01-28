340da33c5197527730bf92b7eefd72f161b67a08
gabrielcho <gabrielarango15@gmail.com>
2023-01-28 15:33:11 -0500

    Debugging cleanup

--- src/middleware/isAuthenticated.js
+++ src/middleware/isAuthenticated.js
@@ -1,10 +1,8 @@
 const ensureAuthenticated = (req, res, next) => {
-    if (req.isAuthenticated()) {
-      return next();
-    }
-    console.log(req.user)
-    console.log('not authenticated')
-    res.send({message: 'Not logged in'});
-  };
-  
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.send({message: 'Not logged in'});
+};
+
 module.exports = ensureAuthenticated;
