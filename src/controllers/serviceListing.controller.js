6d1bd639df5e25543dfbea96444dadd77598599d
gabrielcho <gabrielarango15@gmail.com>
2023-01-31 03:44:58 -0500

    Correction on json response for getServiceListings

--- src/controllers/serviceListing.controller.js
+++ src/controllers/serviceListing.controller.js
@@ -4,5 +4,5 @@ const { getServiceListings } = require('../services/serviceListing.service');
 exports.getServiceListings = async(req, res) => {
     const serviceListings = await getServiceListings();
 
-    res.status(200).json(serviceListings)
+    res.status(200).json({serviceListings: serviceListings})
 }
